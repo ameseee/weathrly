5606d77298df8b0fed9c1e78ee7350598cdfe60e
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-09-19 17:58:14 -0600

    break out card comp

--- test/Search.test.js
+++ test/Search.test.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import Search from '../lib/components/Search';
+import Current from '../lib/components/Current';
+import Welcome from '../lib/components/Welcome';
+import App from '../lib/App';
 
 describe('SEARCH', () => {
 
+//this test should be in welcome
   it('should render search bar in Welcome', () => {
     const component = mount(<Welcome />);
-
+    const current = component.find('.search-div');
+    expect(current.nodes.length).toEqual(1);
   });
 
+//this should be in APP
   it('should render the search bar when displaying weather report', () => {
-    const component = mount(<Current />);
-
+    const component = mount(<App />);
+    const current = component.find('.search-div');
+    console.log(component.debug());
+    expect(current.nodes.length).toEqual(1);
   });
+
 });
